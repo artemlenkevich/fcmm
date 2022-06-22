7879fea2dfd64f89f76f7c5f4308e996a41a7719
Artem Lenkevich <ar.lenkevich@gmail.com>
2022-06-22 15:42:11 +0300

    feat(Modals): added centralized positioning

--- src/components/Modals/Modals.tsx
+++ src/components/Modals/Modals.tsx
@@ -2,6 +2,10 @@ import styles from './Modals.module.scss';
 
 export const Modals: React.FC = () => {
     return (
-        <div className={styles.modals}>Modals</div>
+        <div className={styles.modals}>
+            <div className={styles.modals__container}>
+                <div className={styles.box}></div>
+            </div>
+        </div>
     )
 }
