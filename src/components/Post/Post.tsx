725f8a2f90f511323fd8985b61df1b5b7b0624eb
Artem Lenkevich <ar.lenkevich@gmail.com>
2022-06-22 13:02:33 +0300

    feat(Post): added controls section

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,11 +1,17 @@
 import styles from './Post.module.scss';
-import { ReactComponent as Follow } from './icons/follow.svg';
+import { ReactComponent as FollowIcon } from './icons/follow.svg';
+import { ReactComponent as CommentsIcon } from './icons/comments.svg';
+import { ReactComponent as BookmarkIcon } from './icons/bookmark.svg';
+import { ReactComponent as ShareIcon } from './icons/share.svg';
+import { ReactComponent as ArrowUpIcon } from './icons/arrowup.svg';
+import { ReactComponent as ArrowDownIcon } from './icons/arrowdown.svg';
+import cn from 'classnames';
 
 export const Post: React.FC = () => {
     return (
         <div className={styles.post}>
             <div className={styles.header}>
-                <div className={styles.header__section_left}>
+                <div className={styles.section_left}>
                     <div className={styles.topic}>
                         <img className={styles.topic__icon} src="https://leonardo.osnova.io/1630220b-e511-58f6-8916-826857983f77/-/scale_crop/64x64/" alt="" />
                         <span className={styles.topic__text}>Торговля</span>
@@ -13,9 +19,9 @@ export const Post: React.FC = () => {
                     <div className={styles.creator}>Екатерина Очаковская</div>
                     <div className={styles.creationTime}>вчера</div>
                 </div>
-                <div className={styles.header__section_right}>
+                <div className={styles.section_right}>
                     <button className={styles.subscribe}>
-                        <Follow className={styles.subscribe__icon} />
+                        <FollowIcon className={styles.subscribe__icon} />
                         <span className={styles.subscribe__text}>Подписаться</span>
                     </button>
                 </div>
@@ -24,10 +30,19 @@ export const Post: React.FC = () => {
             <p className={styles.previewText}>С началом «спецоперации» иностранные компании массово покидают Россию, а fashion-ритейл понес, возможно, самые большие потери.</p>
             <img className={styles.contentImage} src="https://leonardo.osnova.io/386456ac-1536-5e02-ba20-f89d7476382f/-/preview/700/-/format/webp/" alt="" />
             <div className={styles.footer}>
-                <div className={styles.commentsCounter}>150</div>
-                <div className={styles.addToBookmarks}></div>
-                <div className={styles.shareBtn}></div>
-                <div className={styles.rank}>9</div>
+                <div className={styles.section_left}>
+                    <div className={styles.commentsCounter}>
+                        <CommentsIcon className={cn(styles.controlIcon, styles.commentsCounter__icon)}/>
+                        <span className={styles.commentsCounter__value}>24</span>
+                    </div>
+                    <BookmarkIcon className={cn(styles.controlIcon, styles.bookmarkIcon)} />
+                    <ShareIcon className={cn(styles.controlIcon, styles.shareIcon)} />
+                </div>
+                <div className={styles.rank}>
+                    <ArrowDownIcon className={styles.controlIcon} />
+                    <div className={styles.rank__value}>6</div>
+                    <ArrowUpIcon className={styles.controlIcon} />
+                </div>
             </div>
         </div>
     )
